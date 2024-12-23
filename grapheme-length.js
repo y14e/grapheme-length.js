7f5f3cc8fd2ef27bcdb58e1a9a52e4c05e57b6ac
Yusuke Kamiyamane <y14e@yusukekamiyamane.com>
2024-12-24 01:14:58 +0900

    Update grapheme-length.js

--- grapheme-length.js
+++ grapheme-length.js
@@ -2,11 +2,3 @@
 String.prototype.graphemeLength = function () {
   return [...new Intl.Segmenter().segment(this)].length;
 };
-
-console.log('computer'.graphemeLength());
-console.log('ｺﾝﾋﾟｭｰﾀｰ'.graphemeLength());
-console.log('ｼﾞｮｺﾋﾞｯﾁ'.graphemeLength());
-console.log('ふんがふんが'.graphemeLength());
-console.log('👩‍❤️‍👩'.graphemeLength());
-console.log('👨‍🦰'.graphemeLength());
-console.log('🐻'.graphemeLength());
